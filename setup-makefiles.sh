LineageOS/android_device_sony_pdx203

pdx203: Commonize blobs shared by the device family
Signed-off-by: Luofan Chen <clfbbn@gmail.com>
Change-Id: Iceee86e85916ff1c2094899e9e6305d118c8f31f

setup-makefiles.sh
@@ -8,30 +8,8 @@
 
 set -e
 
-DEVICE_COMMON=pdx203
-VENDOR=sony
+export DEVICE_COMMON=pdx203
+export DEVICE_COMMON=sm8350-common
+export VENDOR=sony
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
-
-# Copyright headers and common guards
-write_headers "pdx203"
-
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

LineageOS/android_device_sony_pdx206

setup-makefiles.sh
@@ -8,30 +8,8 @@
 
 set -e
 
-DEVICE_COMMON=pdx203
-VENDOR=sony
+export DEVICE_COMMON=pdx203
+export DEVICE_COMMON=sm8250-common
+export VENDOR=sony
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
-
-# Copyright headers and common guards
-write_headers "pdx203"
-
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"