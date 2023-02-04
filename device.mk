aaa71e95287a49ab307e492e0ab6c63d8956580f
Luofan Chen <clfbbn@gmail.com>
2023-02-04 20:35:03 +0800

    pdx206: Decommonize device key overlay
    pdx203 does not have assistant key while pdx206 has
    
    Change-Id: I3db1713d7ad46bae9d268ff11593bbfb33491d68

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # HDR
 PRODUCT_COPY_FILES += \
