5df6ba7519056022ff4116ecd69ae4d2b5e1346b
kyasu <kyasu.dev@gmail.com>
2024-10-20 13:33:52 +0900

    pdx206: Switch to py extract utils
    Change-Id: I984158491058286d95d908c77c85afa3327e423d

---
+++ extract-files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/sony/sm8250-common',
+    'hardware/qcom-caf/sm8250',
+    'hardware/qcom-caf/wlan',
+    'hardware/sony',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+module = ExtractUtilsModule(
+    'pdx206',
+    'sony',
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8250-common', module.vendor
+    )
+    utils.run()
